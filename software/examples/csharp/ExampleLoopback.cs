9c37db316f639fb81e63176fbf4e0b9fd9320a14
Olaf Lüke <olaf@tinkerforge.com>
2015-05-07 14:35:54 +0200

    Add C# example

---
+++ software/examples/csharp/ExampleLoopback.cs
@@ -0,0 +1,45 @@
+using Tinkerforge;
+using System;
+
+// In this program we connect RX to TX and to receive
+// the messages that we are sending
+
+class Example
+{
+	private static string HOST = "localhost";
+	private static int PORT = 4223;
+	private static string UID = "XYZ"; // Change to your UID
+
+	static char[] StringToCharArray(string message)
+	{
+		char[] arr = message.ToCharArray();
+		Array.Resize(ref arr, 60);
+		return arr;
+	}
+
+	// Callback function for read callback (parameter has unit cm)
+	static void ReadCB(BrickletRS232 sender, char[] message, byte length)
+	{
+		string str = new string(message);
+		System.Console.WriteLine("message (length: " + length + "): \"" + str + "\"");
+	}
+
+	static void Main() 
+	{
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletRS232 rs232 = new BrickletRS232(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
+
+		// Register read callback to function ReadCB
+		rs232.ReadCallback += ReadCB;
+		rs232.EnableCallback();
+
+		rs232.Write(StringToCharArray("test\n"), 5);
+
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
+		ipcon.Disconnect();
+	}
+}
