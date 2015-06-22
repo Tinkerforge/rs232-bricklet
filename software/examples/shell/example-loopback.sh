c7a5f4b0d28eb7dcbfa80ae1c280ef9ac7e6475e
Matthias Bolte <matthias@tinkerforge.com>
2015-06-22 15:21:41 +0200

    shell: Use array ellipsis in example

--- software/examples/shell/example-loopback.sh
+++ software/examples/shell/example-loopback.sh
@@ -6,10 +6,8 @@ uid=XYZ
 
 tinkerforge call rs232-bricklet $uid enable-read-callback
 
-# We use 4 characters and fill the rest up to reach a total 
-# of 60 chars, as required by the API.
-tinkerforge call rs232-bricklet $uid write t,e,s,t,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9 4
+# write 4 characters
+tinkerforge call rs232-bricklet $uid write t,e,s,t,.. 4
 
 # handle incoming read callback
 tinkerforge dispatch rs232-bricklet $uid read-callback
-
