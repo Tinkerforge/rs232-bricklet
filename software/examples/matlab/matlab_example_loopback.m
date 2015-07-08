2d641fd8f77ab9cc65ea1204d2c2d2cc8ac56149
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-07-08 14:25:17 +0200

    Add MATLAB loopback example

---
+++ software/examples/matlab/matlab_example_loopback.m
@@ -0,0 +1,36 @@
+function matlab_example_loopback()
+    import com.tinkerforge.IPConnection;
+    import com.tinkerforge.BrickletRS232;
+    import java.lang.String;
+
+    % For this example connect the RX1 and TX pin to receive the send message
+
+    HOST = 'localhost';
+    PORT = 4223;
+    UID = 'XYZ'; % Change to your UID
+
+    ipcon = IPConnection(); % Create IP connection
+    rs232 = BrickletRS232(UID, ipcon); % Create device object
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Register read callback to function cb_read
+    set(rs232, 'ReadCallbackCallback', @(h, e) cb_read(e));
+
+    rs232.enableReadCallback();
+    rs232.write(string_to_char_array(String('test')), 4);
+
+    input('Press any key to exit...\n', 's');
+    ipcon.disconnect();
+end
+
+% Callback function for read callback
+function cb_read(e)
+    fprintf('Message (length: %g): %s\n', e.length, e.message);
+end
+
+function char_array = string_to_char_array(message)
+    import java.util.Arrays;
+    char_array = Arrays.copyOf(message.toCharArray(), 60);
+end
