d1c7f72381090cd5f24418b606edde8c62118f0a
Matthias Bolte <matthias@tinkerforge.com>
2015-11-12 17:30:34 +0100

    shell: Stop callback dispatch before exit

--- software/examples/shell/example-loopback.sh
+++ software/examples/shell/example-loopback.sh
@@ -11,3 +11,7 @@ tinkerforge call rs232-bricklet $uid enable-read-callback
 
 # Write "test" string
 tinkerforge call rs232-bricklet $uid write t,e,s,t,.. 4
+
+echo "Press key to exit"; read dummy
+
+kill -- -$$ # Stop callback dispatch in background
