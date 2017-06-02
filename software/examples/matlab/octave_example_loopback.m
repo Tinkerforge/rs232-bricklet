f3cd3d1f8268b8eba3f4dce288534cb9762d5585
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 16:25:27 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_loopback.m
+++ software/examples/matlab/octave_example_loopback.m
@@ -5,8 +5,8 @@ function octave_example_loopback()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your RS232 Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    rs232 = java_new("com.tinkerforge.BrickletRS232", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    rs232 = javaObject("com.tinkerforge.BrickletRS232", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
